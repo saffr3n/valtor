8548db221ac55d7ae6a584ea4a0f14392e025fb1
saffr3n <saffr3n@gmail.com>
2025-07-02 19:54:09 +0300

    refactor: use ts-eslint config(), remove globals
    - Switch to  `typescript-eslint`'s safer `config()` utility
    - Remove redundant `globals` package

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,21 @@
 import js from '@eslint/js';
-import ts from 'typescript-eslint';
+import ts, { config } from 'typescript-eslint';
 import jsdoc from 'eslint-plugin-jsdoc';
-import prettier from 'eslint-config-prettier';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-config-prettier/flat';
 
-export default defineConfig([
+export default config(
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js, jsdoc },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
-  },
-  ts.configs.recommended,
-  {
-    files: ['**/*.{ts,mts,cts}'],
-    ...jsdoc.configs['flat/recommended-typescript-error'],
-  },
-  {
+    files: ['**/*.{js,ts}'],
+    extends: [js.configs.recommended, ts.configs.recommended],
     rules: {
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-unsafe-function-type': 'off',
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [jsdoc.configs['flat/recommended-typescript-error']],
+    rules: {
       'jsdoc/no-undefined-types': 'error',
       'jsdoc/require-param': ['error', { ignoreWhenAllParamsMissing: true }],
       'jsdoc/require-returns': ['error', { publicOnly: true }],
@@ -32,4 +27,4 @@ export default defineConfig([
     },
   },
   prettier,
-]);
+);
